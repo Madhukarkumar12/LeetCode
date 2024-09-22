cc0e08286935b0c15bf2288db75a644faaeb5fde
Madhukarkumar12 <150505032+Madhukarkumar12@users.noreply.github.com>
2024-09-22 17:54:39 +0530

    Time: 8 ms (71.26%), Space: 10.8 MB (86.35%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int>st;
-        ListNode* temp=head;
-        while(temp != NULL){
-            if(st.find(temp) != st.end()){
-                return true;
-            }
-            st[temp]=1;
-            temp=temp->next;
+        // unordered_map<ListNode*,int>st;
+        // ListNode* temp=head;
+        // while(temp != NULL){
+        //     if(st.find(temp) != st.end()){
+        //         return true;
+        //     }
+        //     st[temp]=1;
+        //     temp=temp->next;
+        // }
+        // return false;
+
+
+        // floyd detection technique.....
+        
+        if(head==NULL || head->next==NULL) return false;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(slow != NULL && fast != NULL){
+            fast=fast->next;
+            if(fast != NULL)  fast=fast->next;
+           
+            slow=slow->next;
+            if(fast == slow) return true;
         }
         return false;
+
     }
 };
